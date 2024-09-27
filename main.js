ca73cb5bc79c5323f3e78bf628bfb540197bc778
08062080 <133919628+08062080@users.noreply.github.com>
2024-09-27 08:45:12 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-document.querySelector (`button`);
-const botao = document.querySelector ('button');
-botao.onclick= function () {
-  alert ('vc clicou no botão');
-}
-
 
 function soma(num1, num2) {
     return num1+num2
 }
 console.log(soma(5,2));
 
+function
