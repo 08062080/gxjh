b2696d3955be618530232d47768de38a069b7dc2
08062080 <133919628+08062080@users.noreply.github.com>
2024-09-27 08:42:22 -0300

    ,

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-document.querySelector(`button`);
+document.querySelector (`button`);
 const botao = document.querySelector ('button');
+botao.onclick= function () {
 
-
+}
 
 
 function soma(num1, num2) {
     return num1+num2
 }
 console.log(soma(5,2));
+
+function
